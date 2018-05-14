8de0b15b066aa849e18f08c4a596321be589ccc5
ShubhCool <36810165+ShubhCool@users.noreply.github.com>
2018-05-14 19:08:13 +0530

    Add files via upload

---
+++ titanic/titanic analysis_2.py
@@ -0,0 +1,69 @@
+
+
+# remove warnings
+import warnings
+warnings.filterwarnings('ignore')
+# ---
+
+%matplotlib inline
+import pandas as pd
+pd.options.display.max_columns = 100
+from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
+import numpy as np
+
+pd.options.display.max_rows = 100
+
+data = pd.read_csv('C:\\Users\\shubham\\Desktop\\implement\\train.csv')
+
+data.shape
+data.head()
+data.describe()
+
+data['Age'].fillna(data['Age'].median(), inplace=True)
+
+data.describe()
+
+survived_sex = data[data['Survived']==1]['Sex'].value_counts()
+dead_sex = data[data['Survived']==0]['Sex'].value_counts()
+df = pd.DataFrame([survived_sex,dead_sex])
+df.index = ['Survived','Dead']
+df.plot(kind='bar',stacked=True, figsize=(15,8))
+
+
+figure = plt.figure(figsize=(15,8))
+plt.hist([data[data['Survived']==1]['Age'], data[data['Survived']==0]['Age']], stacked=True, color = ['g','r'],
+         bins = 30,label = ['Survived','Dead'])
+plt.xlabel('Age')
+plt.ylabel('Number of passengers')
+plt.legend()
+
+figure = plt.figure(figsize=(15,8))
+plt.hist([data[data['Survived']==1]['Fare'],data[data['Survived']==0]['Fare']], stacked=True, color = ['g','r'],
+         bins = 30,label = ['Survived','Dead'])
+plt.xlabel('Fare')
+plt.ylabel('Number of passengers')
+plt.legend()
+
+plt.figure(figsize=(15,8))
+ax = plt.subplot()
+ax.scatter(data[data['Survived']==1]['Age'],data[data['Survived']==1]['Fare'],c='green',s=40)
+ax.scatter(data[data['Survived']==0]['Age'],data[data['Survived']==0]['Fare'],c='red',s=40)
+ax.set_xlabel('Age')
+ax.set_ylabel('Fare')
+ax.legend(('survived','dead'),scatterpoints=1,loc='upper right',fontsize=15,)
+
+ax = plt.subplot()
+ax.set_ylabel('Average fare')
+data.groupby('Pclass').mean()['Fare'].plot(kind='bar',figsize=(15,8), ax = ax)
+
+survived_embark = data[data['Survived']==1]['Embarked'].value_counts()
+dead_embark = data[data['Survived']==0]['Embarked'].value_counts()
+df = pd.DataFrame([survived_embark,dead_embark])
+df.index = ['Survived','Dead']
+df.plot(kind='bar', stacked=True, figsize=(15,8))
+
+
+
+
