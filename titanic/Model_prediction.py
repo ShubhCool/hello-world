e5a8d75c758ed6f0b420022338a66c3c9a4ed2bb
ShubhCool <36810165+ShubhCool@users.noreply.github.com>
2019-08-07 22:57:45 +0530

    Add files via upload

--- titanic/Model_prediction.py
+++ titanic/Model_prediction.py
@@ -5,8 +5,8 @@ Created on Fri Aug  2 19:45:52 2019
 
 @author: shubham
 """
-from data_gathering import Data_gathering
-from feature_engineering import Feature_Engineering
+from codes.data_gathering import Data_gathering
+from codes.feature_engineering import Feature_Engineering
 import pickle
 import pandas as pd
 import os
@@ -19,14 +19,15 @@ class Model_Pred:
     Attributes: 
          
     """        
-    def run_test_prediction(self,test_data:pd.DataFrame,save_path:str,pkl_name:str):
+    def run_test_prediction(self,test_data:pd.DataFrame,save_path:str,pkl_name:str,scaling_rqd:bool):
          
         # Load the pickled model 
         model = pickle.load(open(os.path.join(save_path,pkl_name),'rb'))
-        test_data=Feature_Engineering().input_feat_scale(input_data=test_data,train_yes=False)       
+        if scaling_rqd==True:
+            test_data=Feature_Engineering().input_feat_scale(input_data=test_data,train_yes=False)            
         output = model.predict(test_data).astype(int)
         df_output = pd.DataFrame()
-        aux = Data_gathering().load_dataset(file_path=r'/home/shubham/Desktop/open_projects/',file_name='test.csv')
+        aux = Data_gathering().load_dataset(file_path=r'/home/shubham/Desktop/open_projects/data/',file_name='test.csv')
         df_output['PassengerId'] = aux['PassengerId']
         df_output['Survived'] = output
         df_output[['PassengerId','Survived']].to_csv(r'/home/shubham/Desktop/open_projects/pred.csv',index=False)
